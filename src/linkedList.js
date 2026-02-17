ee3317c4ec8f0394fe6f89e8e1fbadf2cf879122
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-02-17 16:53:26 -0500

    feat: Begin implementation of prepend() method

--- src/linkedList.js
+++ src/linkedList.js
@@ -39,6 +39,20 @@ export class LinkedList {
         ++this.#length;
     }
 
+    prepend(value = null) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            ++this.#length;
+            return;
+        }
+
+        let temp = this.head;
+        this.head = node;
+        this.head.next = temp;
+        ++this.#length;
+    }
+
     /**
      * Returns the last node in the Linked List or null if the list is empty.
      * @returns {Node|null} The last Node in the list or null.
